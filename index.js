919922c8ea86715518847be09aaeb4a411363c79
fredrikvang <52349933+fredrikvang@users.noreply.github.com>
2021-02-10 20:16:31 +0100

    added deleteExpiredEntries

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ class FirestoreAdapter {
     await db.collection(this.model).doc(id).delete()
   }
 
-
   async consume(id) {
     const doc = await db.collection(this.model).doc(id).get()
     if (doc.exists) {
@@ -32,38 +31,50 @@ class FirestoreAdapter {
   }
 
   async findByUid(uid) {
-    const snapshot = await db.collection(this.model).where('uid', '==', uid).get()
+    const snapshot = await db
+      .collection(this.model)
+      .where("uid", "==", uid)
+      .get()
     if (snapshot.empty) {
       return undefined
     } else {
       if (snapshot.size !== 1) {
-        console.warn(`Querying ${this.model} for userCode ${uid} returned ${snapshot.size} items (instead of exactly 1)`)
+        console.warn(
+          `Querying ${this.model} for userCode ${uid} returned ${snapshot.size} items (instead of exactly 1)`
+        )
       }
       return snapshot.docs[0].data
     }
   }
 
   async findByUserCode(userCode) {
-    const snapshot = await db.collection(this.model).where('userCode', '==', userCode).get()
+    const snapshot = await db
+      .collection(this.model)
+      .where("userCode", "==", userCode)
+      .get()
     if (snapshot.empty) {
       return undefined
     } else {
       if (snapshot.size !== 1) {
-        console.warn(`Querying ${this.model} for userCode ${userCode} returned ${snapshot.size} items (instead of exactly 1)`)
+        console.warn(
+          `Querying ${this.model} for userCode ${userCode} returned ${snapshot.size} items (instead of exactly 1)`
+        )
       }
       return snapshot.docs[0].data
     }
   }
 
   async upsert(id, payload, expiresIn) {
-    await db.collection(this.model).doc(id).set({
-      ...payload,
-      __expiry: Date.now() + expiresIn * 1000,
-    })
+    await db
+      .collection(this.model)
+      .doc(id)
+      .set({
+        ...payload,
+        __expiry: Date.now() + expiresIn * 1000,
+      })
   }
 
   async revokeByGrantId(grantId) {
-
     const snapshot = await db
       .collection(this.model)
       .where("grantId", "==", grantId)
@@ -73,8 +84,36 @@ class FirestoreAdapter {
     for (id of ids) batch.delete(db.collection(this.collection).doc(id))
     await batch.commit()
   }
-
-
 }
 
-module.exports = FirestoreAdapter
+/**
+ * Utility function to clean up any expired items. Should be called regularly.
+ */
+const deleteExpiredEntries = async function () {
+  const allCollections = await db.listCollections()
+  const oidcCollections = allCollections
+    .map((x) => x.id)
+    .filter((x) => /^oidc-provider-/.test(x))
+  const limit = 50
+  // FOr each collection beginning with 'oidc-provider-'
+  for (collection of oidcCollections) {
+    // Repeat until we retrieve less than 'limit' expired items from the database
+    while (true) {
+      // Get at most 'limit' expired items
+      const docs = await db
+        .collection(collection)
+        .where("__expiry", "<", Date.now())
+        .limit(limit)
+        .get()
+      const retrievedItems = docs.size
+      if (retrievedItems > 0) { // If any were retrieved
+        const batch = db.batch() // start a batch operation
+        docs.forEach((x) => batch.delete(db.collection(collection).doc(x.id)))
+        await batch.commit() // Where each collection->id is deleted.
+      }
+      if (retrievedItems < limit) break // Nothing more to retrieve, break the while loop
+    }
+  }
+}
+
+module.exports = { FirestoreAdapter, deleteExpiredEntries }
